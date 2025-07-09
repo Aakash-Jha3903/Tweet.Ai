d504baacdfa6c5582ef615b3453050f6231e831f
Aakash <aakashjha343@gmail.com>
2025-07-09 15:45:08 +0530

    Recommend Users

--- client/src/components/UserRelated/RecommendUser.jsx
+++ client/src/components/UserRelated/RecommendUser.jsx
@@ -1,34 +1,34 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import {useDispatch} from 'react-redux'
+import { useDispatch } from 'react-redux'
 import { userFollow } from "../../redux/asyncActions/UserAsync";
 const RecommendUser = ({ user }) => {
   const dispatch = useDispatch();
 
   return (
-   
-      <span className="position-relative hover trendlist">
-         <Link className="d-flex"  to={`/${user.username}`}>
+
+    <span className="position-relative hover trendlist">
+      <Link className="d-flex" to={`/${user.username}`}>
         <span>
           <img alt="img" src={user?.avatar} className="authorImage" />
         </span>
         <span className="left-20">
-        
-            <span className="user-list">{user.username}</span>
-       
+
+          <span className="user-list">{user.nickname || user.username}</span>
+
           <span className=" side-name follow-line">
-            {user.bio || "Anonymous bio"}
+            <strong>@ </strong>{user.username }
           </span>
         </span> </Link>
-        {user.i_follow ? 
+      {user.i_follow ?
         <button
-        onClick={() => dispatch(userFollow(user.username))}
-        className="followbtn border-only-btn">Unfollow</button>
-       : <button
-       onClick={() => dispatch(userFollow(user.username))}
-       className="link-tweet followbtn">Follow</button>}
-      </span>
-   
+          onClick={() => dispatch(userFollow(user.username))}
+          className="followbtn border-only-btn mt-0">Unfollow</button>
+        : <button
+          onClick={() => dispatch(userFollow(user.username))}
+          className="link-tweet followbtn">Follow</button>}
+    </span>
+
   );
 };
 export default RecommendUser
